c65c0522c15731c2f85eaf2656b56b1663139d3a
Dennis Schlevoigt <schlevoigt@dimate.de>
2024-04-23 12:44:46 +0200

    Changes for M3-arm

--- build.sh
+++ build.sh
@@ -19,31 +19,27 @@ create_outdir_lipo()
 {
 echo "create_outdir_lipo"
 for lib_x86_64 in `find $GLOBAL_OUTDIR/x86_64 -name "lib*\.a"`; do
-lib_armv7=`echo $lib_x86_64 | sed "s/x86_64/armv7/g"`
 lib_arm64=`echo $lib_x86_64 | sed "s/x86_64/arm64/g"`
 lib=`echo $lib_x86_64 | sed "s/x86_64//g"`
 
-${LIPO} -arch armv7 $lib_armv7 -arch arm64 $lib_arm64 -arch x86_64 $lib_x86_64 -create -output $lib
+${LIPO} -arch arm64 $lib_arm64 -arch x86_64 $lib_x86_64 -create -output $lib
 done
 }
 
-mkdir -p $GLOBAL_OUTDIR/armv7 $GLOBAL_OUTDIR/arm64 $GLOBAL_OUTDIR/x86_64 $GLOBAL_OUTDIR/include/dcmtk
+mkdir -p $GLOBAL_OUTDIR/arm64_ios $GLOBAL_OUTDIR/arm64_sim $GLOBAL_OUTDIR/include/dcmtk
 
 cd $XCODE_DIR
 
 setenv_all
 
-xcodebuild -configuration 'MinSizeRel' -sdk 'iphoneos' clean build ARCHS="armv7" ONLY_ACTIVE_ARCH=NO CONFIGURATION_BUILD_DIR=$IOS_OUTDIR
-mv $IOS_OUTDIR/* $GLOBAL_OUTDIR/armv7/
-
 xcodebuild -configuration 'MinSizeRel' -sdk 'iphoneos' clean build ARCHS="arm64" ONLY_ACTIVE_ARCH=NO CONFIGURATION_BUILD_DIR=$IOS_OUTDIR
-mv $IOS_OUTDIR/* $GLOBAL_OUTDIR/arm64/
+mv $IOS_OUTDIR/* $GLOBAL_OUTDIR/arm64_ios/
 
-xcodebuild -configuration 'MinSizeRel' -sdk 'iphonesimulator' clean build ARCHS="x86_64" ONLY_ACTIVE_ARCH=NO CONFIGURATION_BUILD_DIR=$SIM_OUTDIR
-mv $SIM_OUTDIR/* $GLOBAL_OUTDIR/x86_64/
+xcodebuild -configuration 'MinSizeRel' -sdk 'iphonesimulator' clean build ARCHS="arm64" ONLY_ACTIVE_ARCH=NO CONFIGURATION_BUILD_DIR=$SIM_OUTDIR
+mv $SIM_OUTDIR/* $GLOBAL_OUTDIR/arm64_sim/
 
 create_outdir_lipo
 
-rm -Rf $GLOBAL_OUTDIR/armv7 $GLOBAL_OUTDIR/arm64 $GLOBAL_OUTDIR/x86_64
+#rm -Rf $GLOBAL_OUTDIR/arm64 $GLOBAL_OUTDIR/x86_64
 
 cp -rf $XCODE_DIR/include/dcmtk $GLOBAL_OUTDIR/include
